265d2a2577a69ec19f4d170e119c38e3ac6b2e8d
gmag11 <github@gmartin.net>
2020-11-23 14:26:49 +0100

    Basic example

--- examples/basicExample/basicExample.cpp
+++ examples/basicExample/basicExample.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 
-#include "WifiConfig.h"
+//#include "WifiConfig.h"
+
 #include <ESPNtpClient.h>
 #ifdef ESP32
 #include <WiFi.h>
@@ -15,7 +16,7 @@
 
 #define SHOW_TIME_PERIOD 1000
 
-const PROGMEM char* ntpServer = "es.pool.ntp.org";
+const PROGMEM char* ntpServer = "pool.ntp.org";
 
 boolean syncEventTriggered = false; // True if a time even has been triggered
 NTPSyncEvent_t ntpEvent; // Last triggered event
@@ -24,7 +25,7 @@ void setup() {
     Serial.begin (115200);
     Serial.println ();
     WiFi.begin (YOUR_WIFI_SSID, YOUR_WIFI_PASSWD);
-    NTP.setTimeZone (TZ_Europe_Madrid);
+    NTP.setTimeZone (TZ_Etc_UTC);
     NTP.begin (ntpServer);
 }
 
@@ -32,7 +33,6 @@ void loop() {
     static int last = 0;
 
     if ((millis () - last) >= SHOW_TIME_PERIOD) {
-         //Serial.println(millis() - last);
         last = millis ();
         Serial.println (NTP.getTimeDateStringUs ());
     }
