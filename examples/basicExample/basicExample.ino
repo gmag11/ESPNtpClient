3cc73dca852cf84cd08a85d2c8773e4a6c352c8b
Shahram <Sh.Nourbakhsh@Gmail.com>
2021-12-23 17:53:15 +0330

    Update basicExample.ino

--- examples/basicExample/basicExample.ino
+++ examples/basicExample/basicExample.ino
@@ -1,27 +1,40 @@
 #include <Arduino.h>
 
 //#include "WifiConfig.h"
-
-#define USE_ETHERNET true        /* false: WiFi  or  true: lwIP_Etherner */
+#define USE_ETHERNET      false             /* false: WiFi  or  true: lwIP_Etherner */
+#define USE_STATIC        false             /* false: DHCP  or  true: STATIC */
 
 #include <ESPNtpClient.h>
-#ifdef ESP32
-#include <WiFi.h>
-#else
-#include <ESP8266WiFi.h>
 
-#if (USE_ETHERNET)
-  #include <SPI.h>
-  #include <ENC28J60lwIP.h>
+#ifdef ESP32
+  #include <WiFi.h>
+
+  #undef USE_ETHERNET
+  #define USE_ETHERNET      false
+#else
+  #include <ESP8266WiFi.h>
   
-  #define ETH_SS_PIN 4
-  ENC28J60lwIP eth(ETH_SS_PIN);
-#endif
-#endif
+  #if (USE_ETHERNET)
+    #include <SPI.h>
+    #include <ENC28J60lwIP.h>
+    
+    #define ETH_SS_PIN 4
+    ENC28J60lwIP eth(ETH_SS_PIN);
+  #endif
+#endif //ESP32
 
 #ifndef WIFI_CONFIG_H
-#define YOUR_WIFI_SSID "YOUR_WIFI_SSID"
-#define YOUR_WIFI_PASSWD "YOUR_WIFI_PASSWD"
+  #define YOUR_WIFI_SSID "YOUR_WIFI_SSID"
+  #define YOUR_WIFI_PASSWD "YOUR_WIFI_PASSWD"
+  
+  // YOUR_ROUTER_SETTINGS
+  #if (USE_STATIC)
+    IPAddress              apIP(192, 168,   1,  10);
+    IPAddress              gwIP(192, 168,   1,   1);
+    IPAddress              snIP(255, 255, 255,   0);
+    IPAddress             dnIP1(192, 168,   1,   1);
+    IPAddress             dnIP2(  8,   8,   8,   8);
+  #endif
 #endif // !WIFI_CONFIG_H
 
 #define SHOW_TIME_PERIOD 1000
@@ -34,6 +47,7 @@ bool connectionStatus() {
       return WiFi.isConnected ();
   #endif
 }
+
 bool connectionReconnect() {
   #if (USE_ETHERNET)
       return true;
@@ -41,6 +55,7 @@ bool connectionReconnect() {
       return WiFi.reconnect ();
   #endif
 }
+
 IPAddress getDeviceIP() {
   #if (USE_ETHERNET)
       return eth.localIP ();
@@ -57,10 +72,23 @@ void setup() {
         SPI.begin();
         
         eth.setDefault();     // use ethernet for default route
+        
+        #if (USE_STATIC)
+          eth.config(apIP, gwIP, snIP, dnIP1, dnIP2);
+        #endif
+        
         eth.begin();
+        
     #else
+        WiFi.mode (WIFI_STA);
+        
+        #if (USE_STATIC)
+          WiFi.config(apIP, gwIP, snIP, dnIP1, dnIP2);
+        #endif
+        
         WiFi.begin (YOUR_WIFI_SSID, YOUR_WIFI_PASSWD);
-    #endif
+        
+    #endif //USE_ETHERNET
     
     NTP.setTimeZone (TZ_Etc_UTC);
     NTP.begin ();
